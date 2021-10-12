f783fd81ff79a2c7f10aa50dc15f1db113e4f88a
Marek Michura <marekti012@gmail.com>
2021-10-12 14:55:44 +0200

    Pierwsza lekcja

---
+++ Aplikacja/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using Aplikacja.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplikacja.Controllers
+{
+    public class LoginController : Controller
+    {
+        static private List<User> Users = new List<User>();
+
+        public IActionResult Index()
+        {
+            return View(model: Users);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Register(User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Users.Add(user);
+                return View("Index", Users);
+            }
+            else
+            {
+                return View("Add");
+            }
+        }
+
+        public IActionResult Remove(int id)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Aplikacja/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplikacja.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Wymagane jest ID")]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Wymagana jest nazwa użytkownika")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Wymagane jest hasło")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Wymagany jest email")]
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}", ErrorMessage = "Niepoprawny email")]
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; } = DateTime.Now;
+    }
+}
